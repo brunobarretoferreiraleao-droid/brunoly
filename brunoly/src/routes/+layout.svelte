4f0d97899199516675ef882cd6a5c62d6abbeb61
LeZucchinni (bruno barreto ferreira-leão) <brunobarretoferreiraleao@gmail.com>
2026-05-15 07:19:37 -0300

    Fix import paths to use lowercase filenames

--- brunoly/src/routes/+layout.svelte
+++ brunoly/src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-  import Background from '$lib/components/layout/Background.svelte';
+  import Background from '$lib/components/layout/background.svelte';
   import MouseGlow from '$lib/components/layout/MouseGlow.svelte';
-  import ClickRipple from '$lib/components/layout/ClickRipple.svelte';
+  import ClickRipple from '$lib/components/layout/clickRipple.svelte';
   import ClickBubbles from '$lib/components/layout/ClickBubbles.svelte';
-  import Navbar from '$lib/components/layout/Navbar.svelte';
+  import Navbar from '$lib/components/layout/navbar.svelte';
 
   import { onMount } from 'svelte';
   import Lenis from 'lenis';
